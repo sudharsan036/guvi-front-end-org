98d148c218d13049b1090d9f8f0201c9f15845c1
sudharsan036 <rsansudha798@gmail.com>
2024-11-04 12:34:07 +0530

    url changes

--- detail-script.js
+++ detail-script.js
@@ -11,7 +11,7 @@ function getQueryParams() {
   async function fetchMovieDetails(movieId) {
     try {
       const response = await fetch(
-        `https://ticketbuy-backend.onrender.com/movie/${movieId}`
+        `https://learn-with-guvi-book-my-show-backend.onrender.com/movie/${movieId}`
       );
       const movie = await response.json();
   
@@ -119,7 +119,7 @@ function getQueryParams() {
   
     try {
       const response = await fetch(
-        "https://ticketbuy-backend.onrender.com/movie/book-movie",
+        "https://learn-with-guvi-book-my-show-backend.onrender.com/movie/book-movie",
         {
           method: "POST",
           headers: {
